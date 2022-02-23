31b6d7f156b2fbfd8af2b3996b38f899e7c004d9
HieuDD <mhoguty0213@gmail.com>
2022-02-23 17:20:17 +0700

    Revert "cros"
    This reverts commit 713b89342888a3151c8b63328ecdeda3767a378c.

--- BE/index.js
+++ BE/index.js
@@ -6,6 +6,7 @@ app.use(cors());
 
 const port = 3000;
 const route = require('../BE/app/routers');
+var account_router= require('./app/routers/account_router');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 route(app);
