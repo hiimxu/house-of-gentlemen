44299a02581886c8339d4d2384152c17c1284d61
duy <Example@gmail.com>
2022-03-14 20:27:58 +0700

    fix get all salon

--- BE/app/models/salonOwner.model.js
+++ BE/app/models/salonOwner.model.js
@@ -40,7 +40,17 @@ SalonOwner.getProfileSalon =function (id, result) {
     });
 }
 SalonOwner.getProfileAllSalon =function (result) {
-    db.query("SELECT * FROM swp490_g11.salonowner where possibility=1", (err, rows, fields) => {
+    db.query(`with temp AS(SELECT swp490_g11.salonowner.salonId,swp490_g11.salonowner.nameSalon,swp490_g11.salonowner.phone,swp490_g11.salonowner.taxCode,swp490_g11.address.detailAddress
+        FROM swp490_g11.salonowner
+        LEFT JOIN swp490_g11.address
+        on swp490_g11.salonowner.salonId=swp490_g11.address.salonId
+        where swp490_g11.salonowner.possibility=1
+        )
+        select temp.salonId,temp.nameSalon,temp.phone,temp.taxCode,temp.detailAddress,swp490_g11.image_salon.image
+        from temp
+        left join swp490_g11.image_salon
+        on temp.salonId=swp490_g11.image_salon.salonId
+        group by salonId`, (err, rows, fields) => {
         if (err) {
            
             result(null,err);
