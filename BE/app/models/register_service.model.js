0a1b6d4716195b66df0e51ab897fc163ccf63ad1
duy <Example@gmail.com>
2022-04-13 21:53:13 +0700

    fix get history booking

--- BE/app/models/register_service.model.js
+++ BE/app/models/register_service.model.js
@@ -128,7 +128,7 @@ Register_service.historyBooking=function(id,result){
     db.query(`with t as (
         select swp490_g11.register_service.registerServiceId,swp490_g11.register_service.serviceId,swp490_g11.register_service.salonId,swp490_g11.register_service.timeRegister,swp490_g11.register_service.timeUse,swp490_g11.register_service.price_original,
         swp490_g11.status_register_service.name as 'nameStatus',swp490_g11.staff.name as 'nameStaff',swp490_g11.register_service.staffId,swp490_g11.salonowner.nameSalon,swp490_g11.service.name as 'nameService',swp490_g11.service.service_time,
-        swp490_g11.salonowner.phone as 'phoneSalon',swp490_g11.address.detailAddress
+        swp490_g11.salonowner.phone as 'phoneSalon',swp490_g11.address.detailAddress,swp490_g11.register_service.note
         from swp490_g11.register_service
         left join swp490_g11.status_register_service
         on swp490_g11.register_service.status_register_id=swp490_g11.status_register_service.status_register_id
@@ -144,7 +144,7 @@ Register_service.historyBooking=function(id,result){
         )
         select t.registerServiceId,t.serviceId,t.salonId,t.timeRegister,t.timeUse,t.price_original,
         t.nameStatus,t.nameStaff,t.staffId,t.nameSalon,t.nameService,swp490_g11.image_service.image,t.service_time,
-        t.phoneSalon,t.detailAddress
+        t.phoneSalon,t.detailAddress,t.note
         from t
         left join swp490_g11.image_service
         on t.serviceId=swp490_g11.image_service.serviceId
