0d5335105703eed7ab96af0a1dee3faed7e3a276
HieuDD <mhoguty0213@gmail.com>
2022-05-20 22:39:24 +0700

    fixed res data for salon booking

--- BE/app/controllers/service.controller.js
+++ BE/app/controllers/service.controller.js
@@ -237,7 +237,7 @@ exports.getServiceOfSalonByAdmin = function (req, res, next) {
                 res.status(400).json({ data: data, message: "get service fail" });
             } else {
                 if (data.length == 0) {
-                    res.status(400).json({ data: data, message: "not have service" });
+                    res.json({ dataSalon: dataSalon, data: data, message: "not have service" });
                 } else {
 
                     res.json({ dataSalon: dataSalon, data: data, message: "get service success" });
